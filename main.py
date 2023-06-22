c7b9a0178b4c54fe112d6c81342388221d6e0f19
Kishor Balgi <kishorbalgi@gmail.com>
2023-06-22 21:15:24 +0530

    feat: Preprocessed the data

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score,KFold,GridSearchCV,RandomizedSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
 # import csv file fetal_health.csv 
 df = pd.read_csv('fetal_health.csv')
+
+# Preprocessing data:
+# 1. Check for missing values
+# print(df.isna().sum().sort_values())
+
+# 2. Seperate features and target:
+X = df.drop('fetal_health', axis=1)
+y = df['fetal_health']
+
+# 3. Split data into train and test sets:
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12,stratify=y)
+
+# 4. Scale data:
+scaler=StandardScaler()
+x_train_scaled=scaler.fit_transform(X_train)
+x_test_scaled=scaler.transform(X_test)
+
+
+
